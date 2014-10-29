57c21971cc8e7b9cc3b50c0db2a58448f27fecdf
Xi Chen <xichen827@gmail.com>
2014-10-28 18:00:26 -0700

    add member route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :cats
-  resources :cat_rental_requests
+  resources :cat_rental_requests do
+    post "approve", to: "cat_rental_requests#approve"
+  end
 end
